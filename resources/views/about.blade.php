7d60a521d5669e60cf00c97586744253f9568a64
karinar4 <karinarahmawati219@gmail.com>
2024-09-12 03:48:49 +0700

    feat: Blade component

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <h1>About</h1>
-    <h3>Owner: {{ $nama }}</h3>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <p>Nama: {{ $nama }}</p>
+</x-layout>
