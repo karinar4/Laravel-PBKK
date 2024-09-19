834e6053f471de2f726601bdceda941aab0a2386
karinar4 <karinarahmawati219@gmail.com>
2024-09-19 10:26:57 +0700

    feat: view data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,48 @@ Route::get('/about', function () {
     return view('about', ['nama' => 'Karina Rahmawati', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Karina Rahmawati',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur commodi ullam ratione adipisci fugiat cupiditate, repellat illum, amet, ea quo exercitationem consequatur et omnis natus dicta suscipit magnam maiores. Dolore?'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Karina Rahmawati',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quisquam accusamus numquam in quae, odit doloribus modi obcaecati excepturi maiores! Optio neque enim cum eaque quis debitis similique mollitia est!'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Karina Rahmawati',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur commodi ullam ratione adipisci fugiat cupiditate, repellat illum, amet, ea quo exercitationem consequatur et omnis natus dicta suscipit magnam maiores. Dolore?'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Karina Rahmawati',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quisquam accusamus numquam in quae, odit doloribus modi obcaecati excepturi maiores! Optio neque enim cum eaque quis debitis similique mollitia est!'
+        ]
+    ];
+ 
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
