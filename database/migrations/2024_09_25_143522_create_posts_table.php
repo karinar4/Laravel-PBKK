d340791d8818bcf3d9e55af2399ad32eed0ef74c
karinar4 <karinarahmawati219@gmail.com>
2024-09-29 15:40:32 +0700

    feat: model factories

--- database/migrations/2024_09_25_143522_create_posts_table.php
+++ database/migrations/2024_09_25_143522_create_posts_table.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**tren-fashion-ramah-lingkungan
+    /**
      * Run the migrations.
      */
     public function up(): void
@@ -14,7 +14,10 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('author');
+            $table->foreignId('author_id')->constrained(
+                table: 'users',
+                indexName: 'posts_author_id'
+            );
             $table->string('slug')->unique();
             $table->text('body');
             $table->timestamps();
