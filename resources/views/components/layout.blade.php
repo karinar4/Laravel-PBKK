55014c3084f9dccc10540d91d126706a8de2f9cb
karinar4 <karinarahmawati219@gmail.com>
2024-09-30 11:33:03 +0700

    feat: searching

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,18 +9,18 @@
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <title>Home</title>
 </head>
-<body class="h-full">
-<div class="min-h-full">
-    <x-navbar></x-navbar>
-  
-    <x-header>{{ $title }}</x-header>
 
-    <main>
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        {{ $slot }}
-      </div>
-    </main>
-  </div>
-  
+<body class="h-full">
+    <div class="min-h-full">
+        <x-navbar></x-navbar>
+    
+        <x-header>{{ $title }}</x-header>
+
+        <main>
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                {{ $slot }}
+            </div>
+        </main>
+    </div>
 </body>
 </html>
